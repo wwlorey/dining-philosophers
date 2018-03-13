442d9412512b47122f903b799d87c1bb95eeac66
Christian Parsons <cpfz9@mst.edu>
2018-03-12 22:23:52 -0500

    Code cleanup

--- baseline.cpp
+++ baseline.cpp
@@ -1,6 +1,6 @@
 /**
  * Christian Parsons, William Lorey
- * Section A
+ * CS3800 Section A
  * Dining Philosophers problem solution using message passing
  */
 
@@ -19,66 +19,104 @@ const unsigned int EXIT = 2;
 
 using namespace std;
 
-//this is how many poems you want each Phil to construct & save
-const int MAXMESSAGES = 10;
+// this is how many poems you want each Phil to construct & save
+const int MAXMESSAGES = 20;
 
-//if you change this base, update the Makefile "clean" accordingly to remove old poems
 const string fileBase = "outFile";
 
-int main ( int argc, char *argv[] )
+void handleReceiveMessage(const int message, bool &fileAvailable)
 {
-  int id; //my MPI ID
-  int p;  //total MPI processes
+  switch (message)
+  {
+    case EXIT:
+      // Our neighbor finished writing. We no longer need to relinquish our fork
+      // when we're finished, so we say it's not available
+      fileAvailable = false;
+      break;
+    case NOTIFY:
+      // Business as usual. We received notice that we can use our fork
+      fileAvailable = true;
+      break;
+  }
+  return;
+}
+
+void write(ofstream &foutLeft, ofstream &foutRight, pomerize &P)
+{
+  // construct poem & output stanzas into the files 'simultaneously'
+  // we do this with an intermediate variable so both files contain the same poem!
+  string stanza1, stanza2, stanza3;
+
+  stanza1 = P.getLine();
+  foutLeft << stanza1 << endl;
+  foutRight << stanza1 << endl;
+
+  stanza2 = P.getLine();
+  foutLeft << stanza2 << endl;
+  foutRight << stanza2 << endl;
+
+  stanza3 = P.getLine();
+  foutLeft << stanza3 << endl << endl;
+  foutRight << stanza3 << endl << endl;
+  return;
+}
+
+int main(int argc, char *argv[])
+{
+  int id; // my MPI ID
+  int p;  // total MPI processes
   MPI::Status status;
   int tag = 1;
+  int source;
 
   bool rightAvailable; // my right fork's (file's) availability
-  bool leftAvailable; // my left fork's availability 
-  bool used = true; // Every fork is 'used' initially
+  bool leftAvailable;  // my left fork's availability
+  bool used = true;    // Every fork is 'used' initially
 
+  // Initialize MPI.
+  MPI::Init(argc, argv);
 
-  //  Initialize MPI.
-  MPI::Init ( argc, argv );
+  // Get the number of total processes.
+  p = MPI::COMM_WORLD.Get_size();
 
-  //  Get the number of total processes.
-  p = MPI::COMM_WORLD.Get_size ( );
-
-  //  Determine the rank of this process.
-  id = MPI::COMM_WORLD.Get_rank ( );
-
+  // Determine the rank of this process.
+  id = MPI::COMM_WORLD.Get_rank();
 
   // Initialize availability depending on the phil's ID
-  if (id == 0) {
+  if (id == 0)
+  {
     // 0th phil doesn't start with any forks :(
     rightAvailable = false;
     leftAvailable = false;
   }
 
-  else if (id == p - 1) {
+  else if (id == p - 1)
+  {
     // The last phil gets both forks
     rightAvailable = true;
     leftAvailable = true;
   }
 
-  else {
+  else
+  {
     // Every other phil gets their left fork
     rightAvailable = false;
     leftAvailable = true;
   }
 
-
-  //Safety check - need at least 2 philosophers to make sense
-  if (p < 2) {
-    MPI::Finalize ( );
+  // Safety check - need at least 2 philosophers to make sense
+  if (p < 2)
+  {
+    MPI::Finalize();
     std::cerr << "Need at least 2 philosophers! Try again" << std::endl;
-    return 1; //non-normal exit
+    return 1; // non-normal exit
   }
 
-  srand(id + time(NULL)); //ensure different seeds...
+  srand(id + time(NULL)); // ensure different seeds...
 
   int numWritten = 0;
 
-  //setup message storage locations
+  // setup message storage locations
   int msgIn, msgOut;
   int leftNeighbor = (id == 0 ? p - 1 : id - 1); // compute left neighbor by getting the previous phil's ID (includes wrap-around)
   int rightNeighbor = (id + 1) % p;
@@ -87,86 +125,64 @@ int main ( int argc, char *argv[] )
 
   string lFile = fileBase + to_string(leftNeighbor);
   string rFile = fileBase + to_string(rightNeighbor);
-  ofstream foutLeft(lFile.c_str(), ios::out | ios::app );
-  ofstream foutRight(rFile.c_str(), ios::out | ios::app );
+  ofstream foutLeft(lFile.c_str(), ios::out | ios::app);
+  ofstream foutRight(rFile.c_str(), ios::out | ios::app);
 
-  while (numWritten < MAXMESSAGES) {
-    if (!(leftAvailable && rightAvailable && used)) {
+  while (numWritten < MAXMESSAGES)
+  {
+    if (!leftAvailable || !rightAvailable || !used)
+    {
 
       // At this point, we can't write to our files. Ask for access
 
-      if (!leftAvailable) {
+      if (!leftAvailable)
+      {
         // Request left fork
         MPI::COMM_WORLD.Send(&REQUEST, 1, MPI::INT, leftNeighbor, tag);
         // Wait for response
         MPI::COMM_WORLD.Recv(&msgIn, 1, MPI::INT, leftNeighbor, tag, status);
 
-        if (msgIn == EXIT) {
-          // The phil to our left is finished writing. We no longer need to relinquish our left fork when we're finished,
-          // so we say it's not available
-          leftAvailable = false;
-        }
-
-        else {
-          // Business as usual. We received notice that we can use our left fork
-          leftAvailable = true;
-        }
+        handleReceiveMessage(msgIn, leftAvailable);
       }
 
-      if (!rightAvailable) {
+      if (!rightAvailable)
+      {
         // Request right fork
         MPI::COMM_WORLD.Send(&REQUEST, 1, MPI::INT, rightNeighbor, tag);
         // Wait for response
         MPI::COMM_WORLD.Recv(&msgIn, 1, MPI::INT, rightNeighbor, tag, status);
 
-        if (msgIn == EXIT) {
-          rightAvailable = false;
-        }
-
-	else {
-          rightAvailable = true;
-        }
+        handleReceiveMessage(msgIn, rightAvailable);
       }
     }
 
     used = false; // We have not yet 'used' our forks
 
-    /* BEGIN WRITE */
-    //construct poem & output stanzas into the files 'simultaneously'
-    //we do this with an intermediate variable so both files contain the same poem!
-    string stanza1, stanza2, stanza3;
-    stanza1 = P.getLine();
-    foutLeft << stanza1 << endl;
-    foutRight << stanza1 << endl;
-
-    stanza2 = P.getLine();
-    foutLeft << stanza2 << endl;
-    foutRight << stanza2 << endl;
-
-    stanza3 = P.getLine();
-    foutLeft << stanza3 << endl << endl;
-    foutRight << stanza3 << endl << endl;
+    write(foutLeft, foutRight, P);
     numWritten++;
-    /* END WRITE */
 
     used = true; // We have 'used' our forks
 
     // Handle message requests
-    while (leftAvailable || rightAvailable) {
+    while (leftAvailable || rightAvailable)
+    {
       MPI::COMM_WORLD.Recv(&msgIn, 1, MPI::INT, MPI::ANY_SOURCE, tag, status);
 
-      if (status.Get_source() == leftNeighbor) {
-	// We've given away our left fork
+      source = status.Get_source();
+      if (source == leftNeighbor)
+      {
+        // We've given away our left fork
         leftAvailable = false;
 
         // Determine if we're finished sending messages or not
         msgOut = (numWritten == MAXMESSAGES ? EXIT : NOTIFY);
 
-	// Inform our left neighbor that they can use the fork
+        // Inform our left neighbor that they can use the fork
         MPI::COMM_WORLD.Send(&msgOut, 1, MPI::INT, leftNeighbor, tag);
       }
 
-      else if (status.Get_source() == rightNeighbor) {
+      else if (source == rightNeighbor)
+      {
         rightAvailable = false;
 
         msgOut = (numWritten == MAXMESSAGES ? EXIT : NOTIFY);
@@ -180,6 +196,6 @@ int main ( int argc, char *argv[] )
   foutRight.close();
 
   //  Terminate MPI.
-  MPI::Finalize ( );
+  MPI::Finalize();
   return 0;
 }
